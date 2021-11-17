e0e0920f4f48c844048bafed4daa817d06c10c3c
whyour <imwhyour@gmail.com>
2021-11-17 23:20:57 +0800

    增加随机电影图片

---
+++ src/interceptor/method/movie/index.ts
@@ -0,0 +1,24 @@
+/**
+ * 随机电影图片功能
+ */
+
+import Interceptor from "../../Interceptor";
+import { FileBox } from 'wechaty';
+
+const jokeInterceptor = new Interceptor("movie", context => {
+    context.template.add("movie.success", [
+        "{content}",
+    ])
+    context.template.add("movie.failed", "哎呀，狗蛋没找到合适的电影图片。等会再来试试？")
+})
+    .title("诗词")
+    .usage("来首诗")
+    .check((context, message) => /^(狗蛋.*)?(电影)/.test(message.text()))
+    .handler(async (context, message) => {
+        const random = Math.floor(Math.random() * (400 - 1)) + 1;
+        const fileBox = FileBox.fromUrl(`https://static.game.kd19.com/fknaolidarenImages/caidianyingmingImages/A0${random}.jpg`)
+        await message.say(fileBox)
+        return '';
+    })
+
+export default jokeInterceptor

e200a020ded2e8a6ee519dbc0f8ab17606cf995c
whyour <imwhyour@gmail.com>
2022-02-10 20:38:55 +0800

    修复telegram转发

--- src/interceptor/method/telegram/api.ts
+++ src/interceptor/method/telegram/api.ts
@@ -1,6 +1,5 @@
 import Axios, { AxiosResponse } from "axios";
-import { exec } from 'child_process';
-
+import { HttpProxyAgent, HttpsProxyAgent } from 'hpagent';
 import { getAPIKey } from "../../../lib/APIs/TelegramAPI";
 
 const axios = Axios.create();
@@ -11,16 +10,44 @@ export async function getGroupIds() {
 }
 
 export async function sendBotMessage(content: string) {
-  const telegramBotProxy = getAPIKey("telegramBotProxy");
+  const telegramBotApiHost = getAPIKey("telegramBotApiHost");
+  const telegramBotProxyAuth = getAPIKey("telegramBotProxyAuth");
+  const telegramBotProxyHost = getAPIKey("telegramBotProxyHost");
+  const telegramBotProxyPort = getAPIKey("telegramBotProxyPort");
   const telegramBotToken = getAPIKey("telegramBotToken");
   const telegramBotUserId = getAPIKey("telegramBotUserId");
 
+  const authStr = telegramBotProxyAuth ? `${telegramBotProxyAuth}@` : '';
+  const url = `https://${telegramBotApiHost ? telegramBotApiHost : 'api.telegram.org'
+    }/bot${telegramBotToken}/sendMessage`;
+  let agent = {};
+  if (telegramBotProxyHost && telegramBotProxyPort) {
+    const options: any = {
+      keepAlive: true,
+      keepAliveMsecs: 1000,
+      maxSockets: 256,
+      maxFreeSockets: 256,
+      proxy: `http://${authStr}${telegramBotProxyHost}:${telegramBotProxyPort}`,
+    };
+    const httpAgent = new HttpProxyAgent(options);
+    const httpsAgent = new HttpsProxyAgent(options);
+    agent = {
+      httpAgent: httpAgent,
+      httpsAgent: httpsAgent,
+    };
+  }
+  
   try {
-    exec(`curl -s -x ${telegramBotProxy} -X POST https://api.telegram.org/bot${telegramBotToken}/sendMessage -F chat_id=${telegramBotUserId} -F text=${content}`, (err, stdout, stderr) => {
-      if (err || stderr) {
-        console.log(`转发tg消息失败：${err || stderr}`)
-      }
-    })
+    const res: any = await axios
+      .post(url, {
+        chat_id: telegramBotUserId,
+        text: content,
+        disable_web_page_preview: true,
+      }, {
+        timeout: 10000,
+        ...agent,
+      });
+    return !!res.data.ok;
   } catch (error) {
     console.log(error)
   }

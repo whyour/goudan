bb0c20164ede08ee6e681efb446554791bc6a216
whyour <imwhyour@gmail.com>
2022-10-08 20:17:42 +0800

    增加repeat

--- src/interceptor/method/qrcode/index.ts
+++ src/interceptor/method/qrcode/index.ts
@@ -9,47 +9,49 @@ import xmldom from 'xmldom';
 import QrScanner from 'qr-scanner';
 
 const qrcodeInterceptor = new Interceptor("qrcode", context => {
-    context.template.add("qrcode.success", [
-        "{content}",
-    ])
-    context.template.add("qrcode.failed", "哎呀，狗蛋获取二维码失败。等会再来试试？")
+  context.template.add("qrcode.success", [
+    "{content}",
+  ])
+  context.template.add("qrcode.failed", "哎呀，狗蛋获取二维码失败。等会再来试试？")
 })
-    .title("二维码")
-    .usage("二维码")
-    .check(async (context, message) => {
-        const text = message.text();
-        let url = '';
-        try {
-            const parser = new xml2js.Parser({ explicitArray: false, ignoreAttrs: true });
-            const result = await parser.parseStringPromise(text);
-            return (result.msg.appmsg.title === 'eq' || result.msg.appmsg.title === 'uq') && result.msg.appmsg.refermsg;
-        } catch (error) { }
-        return false;
-    })
-    .handler(async (context, message, checkerArgs) => {
-        const text = message.text();
-        try {
-            const parser = new xml2js.Parser({ explicitArray: false, ignoreAttrs: true });
-            const result = await parser.parseStringPromise(text);
-            let content = result.msg.appmsg.refermsg.content;
-            const isEncode = result.msg.appmsg.title === 'eq';
+  .title("二维码")
+  .usage("二维码")
+  .check(async (context, message) => {
+    const text = message.text();
+    let url = '';
+    try {
+      const parser = new xml2js.Parser({ explicitArray: false, ignoreAttrs: true });
+      const result = await parser.parseStringPromise(text);
+      if ((result.msg.appmsg.title === 'eq' || result.msg.appmsg.title === 'uq') && result.msg.appmsg.refermsg) {
+        return result
+      }
+    } catch (error) { }
+    return false;
+  })
+  .handler(async (context, message, checkerArgs) => {
+    const text = message.text();
+    try {
+      const parser = new xml2js.Parser({ explicitArray: false, ignoreAttrs: true });
+      const result = checkerArgs
+      let content = result.msg.appmsg.refermsg.content;
+      const isEncode = result.msg.appmsg.title === 'eq';
 
-            let res = content;
-            try {
-                const xmlStringSerialized = new xmldom.DOMParser().parseFromString(content, "text/xml");
-                const refContent = await parser.parseStringPromise(xmlStringSerialized);
-                res = isEncode ? refContent.msg.appmsg && refContent.msg.appmsg.url : refContent.msg;
-            } catch (error) { }
-            
-            if (isEncode) {
-                const fileBox = FileBox.fromUrl(`https://api.oick.cn/qrcode/api.php?text=${encodeURIComponent(res)}`);
-                await message.say(fileBox)
-            } else {
-                const text = await QrScanner.scanImage(res)
-                await message.say(text)
-            }
-        } catch (error) { }
-        return '';
-    })
+      let res = content;
+      try {
+        const xmlStringSerialized = new xmldom.DOMParser().parseFromString(content, "text/xml");
+        const refContent = await parser.parseStringPromise(xmlStringSerialized);
+        res = isEncode ? refContent.msg.appmsg && refContent.msg.appmsg.url : refContent.msg;
+      } catch (error) { }
+
+      if (isEncode) {
+        const fileBox = FileBox.fromUrl(`https://api.oick.cn/qrcode/api.php?text=${encodeURIComponent(res)}`);
+        await message.say(fileBox)
+      } else {
+        const text = await QrScanner.scanImage(res)
+        await message.say(text)
+      }
+    } catch (error) { }
+    return '';
+  })
 
 export default qrcodeInterceptor
---
+++ src/interceptor/method/repeat/index.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { FileBox } from "file-box";
+import Interceptor from "../../Interceptor";
+import { genText, getImage, getMoyuImage } from "./api";
+import fs from "fs";
+import path from "path";
+import { __tmp_dir } from "../../../bot";
+import xml2js from 'xml2js';
+import xmldom from 'xmldom';
+
+const fishInterceptor = new Interceptor("fish")
+  .title("复读机")
+  .alias("repeat")
+  .check(async (context, message) => {
+    const text = message.text();
+    try {
+      const parser = new xml2js.Parser({ explicitArray: false, ignoreAttrs: true });
+      const result = await parser.parseStringPromise(text);
+      if ((result.msg.appmsg.title.startsWith('re ')) && result.msg.appmsg.refermsg) {
+        return { refMsg: result.msg.appmsg.refermsg };
+      }
+    } catch (error) { }
+    return false;
+  })
+  .handler(async (context, message, checkerArgs) => {
+    const text = message.text();
+    const regx = /re (\d+)/;
+    if (regx.test(text)) {
+      const times = parseInt(text.match(regx)[1], 10);
+      for (let i = 0; i < times; i++) {
+        await message.say(checkerArgs.refMsg);
+      }
+    }
+    return '';
+  });
+
+export default fishInterceptor
